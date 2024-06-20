b15e0902f6bbe94627c2e1c67bf1a199323e61d6
elsoazemelet <elsoazemelet@gmail.com>
2024-06-20 12:26:25 +0200

    Fixed package generation

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,22 +1,34 @@
 import typescript from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'es', // ES6 module format
-    sourcemap: true,
+export default [
+  // Configuration for generating JavaScript and type declarations
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'es', // ES6 module format
+      sourcemap: true,
+    },
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.json',
+        useTsconfigDeclarationDir: true,
+      }),
+      nodeResolve(),
+      commonjs(),
+    ],
+    external: ['tslib'],
   },
-  plugins: [
-    typescript({
-      tsconfig: 'tsconfig.json',
-      clean: true,
-      useTsconfigDeclarationDir: true,
-    }),
-    nodeResolve(),
-    commonjs(),
-  ],
-  external: ['tslib'],
-};
+  // Configuration for bundling type declarations
+  {
+    input: './dist/types/index.d.ts',
+    output: {
+      file: './dist/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
